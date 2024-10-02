f5738411c7972cdd848ff2a8780d3410695757a5
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-10-02 06:04:04 +0000

    better

--- Arrays/missingNumber.java
+++ Arrays/missingNumber.java
@@ -1,23 +1,42 @@
+import java.util.*;
 public class missingNumber {
     public static void main(String [] args){
         int nums[]={1,2,4,5};
-        int N=5;
-        boolean pres=false;
+        int N=nums.length;
+        // ##################### BRUTE ##########################
         int mis=0;
-        for(int i=1;i<=nums.length;i++){
-            pres=false;
-            for(int j=0;j<nums.length;j++){
-                if(i==nums[j]){
-                    pres=true;
-                    break;
-                }
+        // boolean pres=false;
+        // for(int i=1;i<=nums.length;i++){
+        //     pres=false;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(i==nums[j]){
+        //             pres=true;
+        //             break;
+        //         }
+        //     }
+        //     if(pres==false){
+        //         mis=i;
+        //         break;
+        //     }
+        // }
+
+
+        // #################################### Better #######################
+        // hashing
+        int hash[]=new int[N+1];
+        Arrays.fill(hash,0);
+        for(int i=0;i<N;i++){
+            if (nums[i] <= N) { 
+                hash[nums[i]] = 1;  
             }
-            if(pres==false){
+        }
+        for(int i=0;i<=N;i++){
+            if(hash[i]==0){
                 mis=i;
-                break;
             }
         }
         System.out.println(mis);
+
     }
     
 }
