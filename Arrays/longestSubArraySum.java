2ebbc1376b54a997ab46ff375a8f4af784800ce8
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-10-04 20:42:15 +0000

    brute done

---
+++ Arrays/longestSubArraySum.java
@@ -0,0 +1,19 @@
+public class longestSubArraySum {
+    public static void main(String args[]){
+        int arr[]={1,2,3,1,1,1,1,4,2,3};
+        int n=10;
+        int k=3;
+        int len=0;
+        int sum;
+        for(int i=0;i<n;i++){
+            sum=0;
+            for(int j=i;j<n;j++){
+                sum+=arr[j];
+                if(sum==k){
+                    len=Math.max(len,j-i+1);
+                }
+            }
+        }
+        System.out.println("Longest subarray: "+len);
+    }
+}
