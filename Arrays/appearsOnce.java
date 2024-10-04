ba2ab6b02be27756871e62dfaea2917ce9fb5bb6
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-10-04 18:17:24 +0000

    better with map

--- Arrays/appearsOnce.java
+++ Arrays/appearsOnce.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 class appearsOnce{
     public static void main(String args[]){
         int arr[]={1,1,2,3,3,4,4};
@@ -18,15 +21,29 @@ class appearsOnce{
 
         // Better 
         // using hash Arrays
-        int hash[]=new int[n+1];
-        for(int i=0;i<n;i++){
-            hash[arr[i]]++;
+        // int hash[]=new int[n+1];
+        // for(int i=0;i<n;i++){
+        //     hash[arr[i]]++;
+        // }
+        // for(int i=0;i<n+1;i++){
+        //     if(hash[i]==1){
+        //         System.out.println("Number that appearsOnce: "+i);
+        //     }
+        // }
+
+        // using map
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int a:arr){
+            int val=map.getOrDefault(a, 0);
+            map.put(a,val+1);
         }
-        for(int i=0;i<n+1;i++){
-            if(hash[i]==1){
-                System.out.println("Number that appearsOnce: "+i);
-            }
+        for (Map.Entry<Integer, Integer> mp : map.entrySet())
+        {
+           if(mp.getValue()==1){
+            System.out.println("Number that appearsOnce: "+mp.getKey());
+           }
         }
+
 
     }
 }
