de9af9473e1d7183e3997a3bae3d1336f919aad0
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-10-04 18:01:18 +0000

    Better using hash array

--- Arrays/appearsOnce.java
+++ Arrays/appearsOnce.java
@@ -1,19 +1,32 @@
 class appearsOnce{
     public static void main(String args[]){
         int arr[]={1,1,2,3,3,4,4};
-        //Brute
         int n=arr.length;
-        int cnt=0;
+        //Brute
+        // int cnt=0;
+        // for(int i=0;i<n;i++){
+        //     cnt=0;
+        //     for(int j=0;j<n;j++){
+        //         if(arr[i]==arr[j]){
+        //             cnt++;
+        //         }
+        //     }
+        //     if(cnt==1){
+        //         System.out.println("Number that appearsOnce: "+arr[i]);
+        //     }
+        // }
+
+        // Better 
+        // using hash Arrays
+        int hash[]=new int[n+1];
         for(int i=0;i<n;i++){
-            cnt=0;
-            for(int j=0;j<n;j++){
-                if(arr[i]==arr[j]){
-                    cnt++;
-                }
-            }
-            if(cnt==1){
-                System.out.println("Number that appearsOnce: "+arr[i]);
+            hash[arr[i]]++;
+        }
+        for(int i=0;i<n+1;i++){
+            if(hash[i]==1){
+                System.out.println("Number that appearsOnce: "+i);
             }
         }
+
     }
 }
