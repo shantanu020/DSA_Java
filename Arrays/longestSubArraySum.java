d68cc24e6f85a2b1513b5e4f1c49259174666152
shantanu020 <146663529+shantanu020@users.noreply.github.com>
2024-10-05 06:29:11 +0000

    Better using hashing done

--- Arrays/longestSubArraySum.java
+++ Arrays/longestSubArraySum.java
@@ -1,19 +1,35 @@
+import java.util.*;
 public class longestSubArraySum {
     public static void main(String args[]){
         int arr[]={1,2,3,1,1,1,1,4,2,3};
         int n=10;
         int k=3;
         int len=0;
-        int sum;
+        int sum=0;
+        // brute
+        // for(int i=0;i<n;i++){
+        //     sum=0;
+        //     for(int j=i;j<n;j++){
+        //         sum+=arr[j];
+        //         if(sum==k){
+        //             len=Math.max(len,j-i+1);
+        //         }
+        //     }
+        // }
+
+        // better(using hashing)- if array contains negative integers then this is best approach
+        Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<n;i++){
-            sum=0;
-            for(int j=i;j<n;j++){
-                sum+=arr[j];
-                if(sum==k){
-                    len=Math.max(len,j-i+1);
-                }
+            sum+=arr[i];
+            if(sum==k){
+                len=i+1;
             }
+            if(map.containsKey(sum-k)){
+                len=Math.max(len,i-map.get(sum-k));
+            }
+            map.putIfAbsent(sum,i);
         }
         System.out.println("Longest subarray: "+len);
+
     }
 }
