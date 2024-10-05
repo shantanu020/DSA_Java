d6e3ebe9d2fc8f8f576e043f1759c9a5893a4357
shantanu020 <shantanuytech@gmail.com>
2024-10-05 23:58:29 +0530

    mazority element via optimal approach using voting algorithm

--- Arrays/mazorityElement.java
+++ Arrays/mazorityElement.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 
 public class mazorityElement {
     public static void main(String[] args) {
@@ -7,15 +7,26 @@ public class mazorityElement {
         // Better (use hashing)
         int arr[]={2,2,1,1,1,2,2};
         int n=7;
-        HashMap<Integer,Integer> map=new  HashMap<>();
-        for(int a:arr){
-            int cnt=map.getOrDefault(a, 0);
-            map.put(a, cnt+1);
-        }
-        for (Map.Entry<Integer, Integer> en : map.entrySet()) {
-            if(en.getValue()>n/2){
-                System.out.println("Mazority Element: "+en.getKey());
+        // HashMap<Integer,Integer> map=new  HashMap<>();
+        // for(int a:arr){
+        //     int cnt=map.getOrDefault(a, 0);
+        //     map.put(a, cnt+1);
+        // }
+        // for (Map.Entry<Integer, Integer> en : map.entrySet()) {
+        //     if(en.getValue()>n/2){
+        //         System.out.println("Mazority Element: "+en.getKey());
+        //     }
+        // }
+
+        // Optimal (moore's Voting Algorithm)
+        int cnt=0;
+        int maz=0;
+        for(int i=0;i<n;i++){
+            if(cnt==0){
+                maz=arr[i];
             }
+            cnt+=(maz==arr[i])?1:-1;
         }
+        System.out.println("Mazority Element: "+maz);
     }
 }
