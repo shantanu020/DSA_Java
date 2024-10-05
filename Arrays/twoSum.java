d34aeb9e7627b96b291128a03a90cdccdba8863b
shantanu020 <shantanuytech@gmail.com>
2024-10-05 16:03:09 +0530

    optimal if to return true/false, yes/no

--- Arrays/twoSum.java
+++ Arrays/twoSum.java
@@ -1,5 +1,4 @@
 
-import java.util.*;
 public class twoSum {
     public static void main(String[] args) {
         int arr[]={2,6,5,8,11};
@@ -15,12 +14,30 @@ public class twoSum {
         // }
         
         // better using hashing (Optimal if we have to return indexes)
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<n;i++){
-            if(map.containsKey(target-arr[i])){
-                System.out.println("Index of first element is: "+i+"\n"+"Index of second element is: "+map.get(target-arr[i]));
+        // Map<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //     if(map.containsKey(target-arr[i])){
+        //         System.out.println("Index of first element is: "+i+"\n"+"Index of second element is: "+map.get(target-arr[i]));
+        //     }
+        //     map.put(arr[i],i);
+        // }
+
+        // Optimal if to return true/false, YES/NO
+
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            if(arr[i]+arr[j]==target){
+                System.out.println("YES");
+                break;
+            }else if(arr[i]+arr[j]>target){
+                j--;
+            }else if (arr[i]+arr[j]<target) {
+                i++;
+            }else{
+                System.out.println("NO");
+                break;
             }
-            map.put(arr[i],i);
         }
     }
 }
