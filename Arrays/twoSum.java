77c6d0250a7c46b90b7f591efb5580e5543fe666
shantanu020 <shantanuytech@gmail.com>
2024-10-05 13:03:16 +0530

    Better using hashing

--- Arrays/twoSum.java
+++ Arrays/twoSum.java
@@ -1,16 +1,26 @@
+
+import java.util.*;
 public class twoSum {
     public static void main(String[] args) {
         int arr[]={2,6,5,8,11};
         int n=5;
         int target=10;
         // Brute force approach TC=O(n*n)
-        for (int i = 0; i < arr.length; i++) {
-            for(int j=i+1;j<n;j++){
-                if(arr[i]+arr[j]==target){
-                    System.out.println("Index of first element is: "+i+"\n"+"Index of second element is: "+j);
-                }
-            }
-        }
+        // for (int i = 0; i < arr.length; i++) {
+        //     for(int j=i+1;j<n;j++){
+        //         if(arr[i]+arr[j]==target){
+        //             System.out.println("Index of first element is: "+i+"\n"+"Index of second element is: "+j);
+        //         }
+        //     }
+        // }
         
+        // better using hashing
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(map.containsKey(target-arr[i])){
+                System.out.println("Index of first element is: "+i+"\n"+"Index of second element is: "+map.get(target-arr[i]));
+            }
+            map.put(arr[i],i);
+        }
     }
 }
