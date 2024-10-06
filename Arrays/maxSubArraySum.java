33b693c989063130ce2eee4c3052ea3134281724
shantanu020 <shantanuytech@gmail.com>
2024-10-06 08:38:00 +0530

    maxSubArraySum via kadanes algorithm , optimal approach

--- Arrays/maxSubArraySum.java
+++ Arrays/maxSubArraySum.java
@@ -2,7 +2,8 @@ public class maxSubArraySum {
     public static void main(String[] args) {
         int arr[]={-2,-3,4,-1,-2,1,5,-3};
         int n=arr.length;
-        System.out.println("MAX SUM: "+brute(arr,n));
+        System.out.println("MAX SUM via BRUTE: "+brute(arr,n));
+        System.out.println("MAX SUM via OPTIMAL: "+kadanesOptimal(arr,n));
         
     }
     public static int brute(int [] arr,int n){
@@ -13,6 +14,20 @@ public class maxSubArraySum {
             for (int j = i; j < n; j++){
                 sum+=arr[j];
                 maxSum=Math.max(maxSum,sum);
+            }
+        }
+        return maxSum;
+    }
+    public static int kadanesOptimal(int [] arr,int n){
+        int sum=0;
+        int maxSum=Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            sum+=arr[i];
+            if(maxSum<sum){
+                maxSum=sum;
+            }
+            if(sum<0){
+                sum=0;
             }
         }
         return maxSum;
