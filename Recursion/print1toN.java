bc2dc0524abfd223849da90ac076418828ff29f9
shantanu020 <shantanuytech@gmail.com>
2024-10-07 20:22:28 +0530

    print1toN using recursion

---
+++ Recursion/print1toN.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class print1toN {
+    public static void main(String[] args) {
+        print(10,1);
+    }
+    public static void print(int n,int i) {
+        if(n==0)
+        {
+            return;
+        } 
+        System.out.print(i+" ");
+        print(n-1,i+1);
+    }
+}
