9fc9a324b372810b0ddac4f1c08ad01ce64f6a41
shantanu020 <shantanuytech@gmail.com>
2024-10-07 21:28:30 +0530

    corrected the logic

--- Recursion/SumOfFirstN.java
+++ Recursion/SumOfFirstN.java
@@ -4,14 +4,14 @@ public class SumOfFirstN {
     public static void main(String[] args) {
         int sum=0;
         int arr[]={1,2,3,4,5};
-        sum=printSum(arr,sum,0);
+        sum=printSum(arr,0);
         System.out.println("Sum: "+sum);
     }
-    public static int printSum(int [] arr,int sum,int i){
+    public static int printSum(int []arr,int i){
         if(i==arr.length){
             return 0;
         }
-        return (int)Math.pow(arr[i],3)+printSum(arr,sum,i+1);
+        return (int)Math.pow(arr[i],3)+printSum(arr,i+1);
     }
 }
 // TC:O(N)
