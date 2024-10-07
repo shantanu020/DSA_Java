3b5825816c7e3b92357e096f12a14f8387285b97
shantanu020 <shantanuytech@gmail.com>
2024-10-07 21:44:51 +0530

    printNto1 by backtracking

--- Recursion/printNto1.java
+++ Recursion/printNto1.java
@@ -3,6 +3,9 @@ package Recursion;
 public class printNto1 {
     public static void main(String[] args) {
         print(10);
+        System.out.println("\n By Backtracking");
+        printBack(10, 1);
+
     }
     public static void print(int n){
         if(n==0){
@@ -10,6 +13,14 @@ public class printNto1 {
         }
         System.out.print(n+" ");
         print(n-1);
+    }
+    // print by backtracking
+    public static void printBack(int n,int i){
+        if(i==n+1){
+            return;
+        }
+        printBack(n,i+1);
+        System.out.print(i+" ");
     }
 }
 // TC: O(N)
