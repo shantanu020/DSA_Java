389309390ebbdb444e1dd0dba6ed91e14e06680f
shantanu020 <shantanuytech@gmail.com>
2024-10-08 19:30:46 +0530

    Fibonacci using recursion

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,14 @@
+package Recursion;
+public class Fibonacci {
+    public static void main(String[] args) {
+       int n =fib(10);
+       System.out.println(n);
+        
+    }
+    public static int fib(int n){
+        if(n<=1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+}
