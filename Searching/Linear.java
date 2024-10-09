cdac19e72a83d948bc1537826c36e545f65edd55
shantanu020 <shantanuytech@gmail.com>
2024-10-09 23:17:49 +0530

    Linear Search if element not found

--- Searching/Linear.java
+++ Searching/Linear.java
@@ -5,10 +5,16 @@ public class Linear {
         int arr[]={1,3,6,7,9,2};
         int n=6;
         int key=9;
+        int flag=0;
         for(int i=0;i<n;i++){
             if(arr[i]==key){
                 System.out.println("Index of key is: "+i);
+                flag=1;
             }
         }
+        if(flag==0){
+            System.out.println("Elemen not found");
+        }
     }
+
 }
